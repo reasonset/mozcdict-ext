3a3ee37c29e8a5e830a1d970da43ef73e2564046
Masato Toyoshima <phoepsilonix@gmail.com>
2023-03-13 01:58:52 +0900

    Add parallel processing. Aimed at saving time, parallel processing was added using the parallel library. Added option to specify paths to external files such as id.def.
    並列処理を追加。
    時間短縮を狙い、parallelライブラリを用いて並列処理を追加しました。
    id.defなど外部ファイルのパスを指定するオプションを追加しました。

--- neologd/neologd.rb
+++ neologd/neologd.rb
@@ -1,100 +1,122 @@
 #! /usr/bin/env ruby
+# coding: UTF-8
 require 'csv'
 require 'nkf'
 require 'optparse'
+require 'parallel'
 
 ID_DEF = {}
 ALREADY = {}
 
+$opts = { threads: 18, slice: 8000,
+          filename: "src/seed/user-dict-seed.csv",
+          idfile: "../../mozc/src/data/dictionary_oss/id.def"
+}
+
+op = OptionParser.new
+op.on("-e", "--english")
+op.on('-tNUM', '--threads=NUM', Integer ) { |v| $opts[:threads] = v }
+op.on('-sNUM', '--slice=NUM', Integer ) { |v| $opts[:slice] = v }
+op.on('-fVAL', '--filename=VAL', String ) { |v| $opts[:filename] = v }
+op.on('-iVAL', '--idfile=VAL', String ){ |v| $opts[:idfile] = v }
+op.parse!(ARGV, into: $opts)
+
 unless ENV["MOZC_ID_FILE"]
-  abort "Mozc ID Definition File is not given."
+  MOZC_ID_FILE=$opts[:idfile]
+else
+  MOZC_ID_FILE=ENV["MOZC_ID_FILE"]
 end
 
 # Load Mozc ID definition.
-File.open(ENV["MOZC_ID_FILE"], "r") do |f|
+File.open(MOZC_ID_FILE, "r") do |f|
   f.each do |line|
     id, expr = line.chomp.split(" ", 2)
     ID_DEF[expr] = id
   end
 end
 
-$opts = {}
-op = OptionParser.new
-op.on("-e", "--english")
-op.parse!(ARGV, into: $opts)
+# parallel
+THREAD_NUM=$opts[:threads]
+SLICE_NUM=$opts[:slice]
 
 # Read CSV each line from file.
-CSV.foreach("src/seed/user-dict-seed.csv") do |row|
+file = CSV.open($opts[:filename], "r:utf-8")
+file.each_slice(SLICE_NUM) do |rows|
   # 表層形,左文脈ID,右文脈ID,コスト,品詞1,品詞2,品詞3,品詞4,品詞5,品詞6,原形,読み,発音
   # #24時間以内に240RT来なければ俺の嫁,1288,1288,3942,名詞,固有名詞,一般,*,*,*,#24時間以内に240RT来なければ俺の嫁,ニジュウヨジカンイナイニニヒャクヨンジュウアールティーコナケレバオレノヨメ,ニジュウヨジカンイナイニニヒャクヨンジュウアールティーコナケレバオレノヨメ
-  surface, lcxid, rcxid, cost, cls1, cls2, cls3, cls4, cls5, cls6, base, kana, pron = *row
+  results = Parallel.map(rows, in_threads: THREAD_NUM) do | row |
+    surface, lcxid, rcxid, cost, cls1, cls2, cls3, cls4, cls5, cls6, base, kana, pron = row
 
-  yomi = NKF.nkf("--hiragana -w -W", kana).tr("ゐゑ", "いえ")
+    yomi = NKF.nkf("--hiragana -w -W", kana).tr("ゐゑ", "いえ")
 
-  # 読みがひらがな以外を含む場合はスキップ => 検証
-  # 名詞以外の場合はスキップ => しない
+    # 読みがひらがな以外を含む場合はスキップ => 検証
+    # 名詞以外の場合はスキップ => しない
 
-  # 「地域」をスキップ。地名は郵便番号ファイルから生成する => 踏襲する
-  next if cls3 == "地域"
+    # 「地域」をスキップ。地名は郵便番号ファイルから生成する => 踏襲する
+    next if cls3 == "地域"
 
-  # 「名」をスキップ => しない
+    # 「名」をスキップ => しない
 
-  clsexpr = [cls1, cls2, cls3, cls4, cls5, cls6].join(",")
-  cost = cost.to_i
+    clsexpr = [cls1, cls2, cls3, cls4, cls5, cls6].join(",").force_encoding('UTF-8')
+    #clsexpr = [cls1, cls2, cls3, cls4, cls5, cls6].join(",")
+    cost = cost.to_i
 
-  ##### List class (develop feature) #####
-  # puts clsexpr
-  # next
+    ##### List class (develop feature) #####
+    # puts clsexpr
+    # next
 
-  # コスト計算の処理はMozc-UTに倣っている
-  mozc_cost = case
-  when cost < 0
-    # コストがマイナスの場合は8000にする
-    8000
-  when cost > 10000
-		# コストが10000を超える場合は10000にする
-    10000
-  else
-		# コストを 6000 < cost < 7000 に調整する
-    6000 + (cost / 10)
+    # コスト計算の処理はMozc-UTに倣っている
+    mozc_cost = case
+                when cost < 0
+                  # コストがマイナスの場合は8000にする
+                  8000
+                when cost > 10000
+                  # コストが10000を超える場合は10000にする
+                  10000
+                else
+                  # コストを 6000 < cost < 7000 に調整する
+                  6000 + (cost / 10)
+                end
+
+    # idは探索を行う。
+    # 既知のNeologdのクラスを変換
+    # 未知のものは無視する
+    id = case clsexpr
+         when "記号,一般,*,*,*,*"
+           ID_DEF["記号,一般,*,*,*,*,*"]
+         when "名詞,固有名詞,人名,一般,*,*"
+           ID_DEF["名詞,固有名詞,人名,一般,*,*,*"]
+         when "名詞,固有名詞,一般,*,*,*", "名詞,固有名詞,一般,*,*,"
+           ID_DEF["名詞,固有名詞,一般,*,*,*,*"]
+         when "名詞,固有名詞,組織,*,*,*"
+           ID_DEF["名詞,固有名詞,組織,*,*,*,*"]
+         when "名詞,一般,*,*,*,*"
+           ID_DEF["名詞,一般,*,*,*,*,*"]
+         when "名詞,サ変接続,*,*,*,*"
+           ID_DEF["名詞,サ変接続,*,*,*,*,*"]
+         when "名詞,固有名詞,人名,名,*,*"
+           ID_DEF["名詞,固有名詞,人名,名,*,*,*"]
+         when "名詞,固有名詞,人名,姓,*,*"
+           ID_DEF["名詞,固有名詞,人名,姓,*,*,*"]
+         end
+
+    #raise unless id      # DEVELOPMENT MODE
+    next unless id
+
+    # 英語への変換はオプションによる (デフォルトスキップ)
+    # 固有名詞は受け入れる
+    next if (!$opts[:english] && base =~ /^[a-zA-Z ]+$/ && !clsexpr.include?("固有名詞") )
+
+    generic_expr = [yomi, id,  base].join(" ")
+    if ALREADY[generic_expr]
+      next
+    else
+      ALREADY[generic_expr] = true
+      line_expr = [yomi, id, id, mozc_cost, base]
+    end
   end
-
-  # idは探索を行う。
-  # 既知のNeologdのクラスを変換
-  # 未知のものは無視する
-  id = case clsexpr
-  when "記号,一般,*,*,*,*"
-    ID_DEF["記号,一般,*,*,*,*,*"]
-  when "名詞,固有名詞,人名,一般,*,*"
-    ID_DEF["名詞,固有名詞,人名,一般,*,*,*"]
-  when "名詞,固有名詞,一般,*,*,*", "名詞,固有名詞,一般,*,*,"
-    ID_DEF["名詞,固有名詞,一般,*,*,*,*"]
-  when "名詞,固有名詞,組織,*,*,*"
-    ID_DEF["名詞,固有名詞,組織,*,*,*,*"]
-  when "名詞,一般,*,*,*,*"
-    ID_DEF["名詞,一般,*,*,*,*,*"]
-  when "名詞,サ変接続,*,*,*,*"
-    ID_DEF["名詞,サ変接続,*,*,*,*,*"]
-  when "名詞,固有名詞,人名,名,*,*"
-    ID_DEF["名詞,固有名詞,人名,名,*,*,*"]
-  when "名詞,固有名詞,人名,姓,*,*"
-    ID_DEF["名詞,固有名詞,人名,姓,*,*,*"]
-  end
-
-  #raise unless id      # DEVELOPMENT MODE
-  next unless id
-
-  # 英語への変換はオプションによる (デフォルトスキップ)
-  # 固有名詞は受け入れる
-  next if (!$opts[:english] && base =~ /^[a-zA-Z ]+$/ && !clsexpr.include?("固有名詞") )
-  
-  line_expr = [yomi, id, id, mozc_cost, base].join("\t")
-  generic_expr = [yomi, id,  base].join(" ")
-  if ALREADY[generic_expr]
-    next
-  else
-    ALREADY[generic_expr] = true
-  end
-
-  puts line_expr
+  results.map{ |x|
+    next if x.nil?
+    puts x.join("\t")
+  }
 end
--- sudachi/sudachi.rb
+++ sudachi/sudachi.rb
@@ -1,8 +1,10 @@
 #!/usr/bin/env ruby
+# coding: UTF-8
 require 'csv'
 require 'nkf'
 require 'yaml'
 require 'optparse'
+require 'parallel'
 
 ##### CONSTANTS #####
 ROUND = !(ENV["WORDCLASS_ROUND"]&.downcase == "no")
@@ -14,92 +16,112 @@ CLASS_MAP = YAML.load(File.read("clsmap.yaml"))
 ID_DEF = {}
 ALREADY = {}
 
+$opts = { threads: 18, slice: 8000,
+          filename: [ "src/core_lex.csv", "src/notcore_lex.csv" ],
+          idfile: "../../mozc/src/data/dictionary_oss/id.def"
+}
+op = OptionParser.new
+op.on("-e", "--english")
+op.on("-s", "--symbol")
+op.on('-tNUM', '--threads=NUM', Integer ) { |v| $opts[:threads] = v }
+op.on('-sNUM', '--slice=NUM', Integer ) { |v| $opts[:slice] = v }
+op.on('-fVAL', '--filename=VAL', String ) { |v| $opts[:filename] = v }
+op.on('-iVAL', '--idfile=VAL', String ){ |v| $opts[:idfile] = v }
+op.parse!(ARGV, into: $opts)
+
 unless ENV["MOZC_ID_FILE"]
-  abort "Mozc ID Definition File is not given."
+  MOZC_ID_FILE=$opts[:idfile]
+else
+  MOZC_ID_FILE=ENV["MOZC_ID_FILE"]
 end
 
 # Load Mozc ID definition.
-File.open(ENV["MOZC_ID_FILE"], "r") do |f|
+File.open(MOZC_ID_FILE, "r") do |f|
   f.each do |line|
     id, expr = line.chomp.split(" ", 2)
     ID_DEF[expr] = id
   end
 end
 
-$opts = {}
-op = OptionParser.new
-op.on("-e", "--english")
-op.on("-s", "--symbol")
-op.parse!(ARGV, into: $opts)
+# parallel
+THREAD_NUM=$opts[:threads]
+SLICE_NUM=$opts[:slice]
 
 # baseball heroes,4785,4785,5000,BASEBALL HEROES,名詞,固有名詞,一般,*,*,*,ベースボールヒーローズ,BASEBALL HEROES,*,A,*,*,*,*
 # 見出し (TRIE 用),左連接ID,右連接ID,コスト,見出し (解析結果表示用), 品詞1,品詞2,品詞3,品詞4,品詞 (活用型),品詞 (活用形), 読み,正規化表記,辞書形ID,分割タイプ,A単位分割情報,B単位分割情報,※未使用
 
-["src/core_lex.csv", "src/notcore_lex.csv"].each do |source_file|
-  CSV.foreach(source_file) do |row|
-    head_trie, lid, rid, cost, head_anal, cls1, cls2, cls3, cls4, cls5, cls6, kana, normal, did, dtype, adiv, bdiv = *row
+#["src/core_lex.csv", "src/notcore_lex.csv"].each do |source_file|
+$opts[:filename].each do |source_file|
+  file = CSV.open(source_file, "r:utf-8")
+  file.each_slice(SLICE_NUM) do |rows|
+    results = Parallel.map(rows, in_threads: THREAD_NUM) do | row |
+      head_trie, lid, rid, cost, head_anal, cls1, cls2, cls3, cls4, cls5, cls6, kana, normal, did, dtype, adiv, bdiv = *row
 
-    # 読みがかなで構成されていないものを除外する
-    next if kana =~ /[^\p{hiragana}\p{katakana}ー]/
+      # 読みがかなで構成されていないものを除外する
+      next if kana =~ /[^\p{hiragana}\p{katakana}ー]/
 
-    yomi = NKF.nkf("--hiragana -w -W", kana).tr("ゐゑ", "いえ")
+      yomi = NKF.nkf("--hiragana -w -W", kana).tr("ゐゑ", "いえ")
 
-    # 見出し (解析結果表示用)を表記とみなす
-    base = head_anal
-    
-    # head_trie と conv_to が casecmp false な例:
-    # ["co・cp共済", "4785", "4785", "15000", "CO･CP共済", "名詞", "固有名詞", "一般", "*", "*", "*", "コープキョウサイ", "コープ共済", "*", "A", "*", "*", "*", "021722"]
-    # Mozcdic-UTではスキップされていたが、こちらは単純に解析結果表示用を採用することとする
-    # next unless head_trie.casecmp(conv_to).zero?
+      # 見出し (解析結果表示用)を表記とみなす
+      base = head_anal
 
-    # 名詞以外の場合はスキップ => しない
-    # 「地名」をスキップ。地名は郵便番号ファイルから生成する => 踏襲
-    next if cls3 == "地名"
-		# 「名」をスキップ => しない
+      # head_trie と conv_to が casecmp false な例:
+      # ["co・cp共済", "4785", "4785", "15000", "CO･CP共済", "名詞", "固有名詞", "一般", "*", "*", "*", "コープキョウサイ", "コープ共済", "*", "A", "*", "*", "*", "021722"]
+      # Mozcdic-UTではスキップされていたが、こちらは単純に解析結果表示用を採用することとする
+      # next unless head_trie.casecmp(conv_to).zero?
 
-    clsexpr = [cls1, cls2, cls3, cls4, cls5, cls6].join(",")
-    cost = cost.to_i
+      # 名詞以外の場合はスキップ => しない
+      # 「地名」をスキップ。地名は郵便番号ファイルから生成する => 踏襲
+      next if cls3 == "地名"
+      # 「名」をスキップ => しない
 
-    # コスト計算の処理はMozc-UTに倣っている
-    mozc_cost = case
-    when cost < 0
-      # コストがマイナスの場合は8000にする
-      8000
-    when cost > 10000
-      # コストが10000を超える場合は10000にする
-      10000
-    else
-      # コストを 6000 < cost < 7000 に調整する
-      6000 + (cost / 10)
+      clsexpr = [cls1, cls2, cls3, cls4, cls5, cls6].join(",")
+      cost = cost.to_i
+
+      # コスト計算の処理はMozc-UTに倣っている
+      mozc_cost = case
+                  when cost < 0
+                    # コストがマイナスの場合は8000にする
+                    8000
+                  when cost > 10000
+                    # コストが10000を超える場合は10000にする
+                    10000
+                  else
+                    # コストを 6000 < cost < 7000 に調整する
+                    6000 + (cost / 10)
+                  end
+
+      ##### List class (develop feature) #####
+      # puts clsexpr
+      # next
+
+      # 既知のクラスの変換
+      id = ID_DEF[CLASS_MAP[clsexpr]]
+
+      # 品詞が特定できないケース
+      if !id
+        ERROR_UNEXPECTED_CLS ? abort("Unexpected Word Class #{clsexpr}") : next
+      end
+
+      # 英語への変換はオプションによる (デフォルトスキップ)
+      # 固有名詞は受け入れる
+      next if (!$opts[:english] && base =~ /^[a-zA-Z ]+$/ && !clsexpr.include?("固有名詞") )
+
+      # 「きごう」で変換される記号は多すぎて支障をきたすため、除外する
+      next if (!$opts[:symbol] && yomi == "きごう" && clsexpr.include?("記号"))
+
+      generic_expr = [yomi, id, base].join(" ")
+      if ALREADY[generic_expr]
+        next
+      else
+        ALREADY[generic_expr] = true
+        line_expr = [yomi, id, id, mozc_cost, base]
+      end
     end
 
-    ##### List class (develop feature) #####
-    # puts clsexpr
-    # next
-
-    # 既知のクラスの変換
-    id = ID_DEF[CLASS_MAP[clsexpr]]
-
-    # 品詞が特定できないケース
-    if !id
-      ERROR_UNEXPECTED_CLS ? abort("Unexpected Word Class #{clsexpr}") : next
-    end
-
-    # 英語への変換はオプションによる (デフォルトスキップ)
-    # 固有名詞は受け入れる
-    next if (!$opts[:english] && base =~ /^[a-zA-Z ]+$/ && !clsexpr.include?("固有名詞") )
-
-    # 「きごう」で変換される記号は多すぎて支障をきたすため、除外する
-    next if (!$opts[:symbol] && yomi == "きごう" && clsexpr.include?("記号"))
-
-    line_expr = [yomi, id, id, mozc_cost, base].join("\t")
-    generic_expr = [yomi, id, base].join(" ")
-    if ALREADY[generic_expr]
-      next
-    else
-      ALREADY[generic_expr] = true
-    end
-  
-    puts line_expr
+    results.map{ |x|
+      next if x.nil?
+      puts x.join("\t")
+    }
   end
 end
