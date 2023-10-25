8b6392e027c7377656457911e192ec52bade632d
Masaki Haruka <yek@reasonset.net>
2023-10-26 06:18:33 +0900

    Use global ver.

--- lib/dictutils.rb
+++ lib/dictutils.rb
@@ -1,18 +1,18 @@
 def load_exclude_dict
-  CONFIG_DIR = File.join((ENV["XDG_CONFIG_HOME"] || [ENV["HOME"], ".config"]), "mozcdic-ext")
-  EXCLUDE_DICT = []
+  $config_dir = File.join((ENV["XDG_CONFIG_HOME"] || [ENV["HOME"], ".config"]), "mozcdic-ext")
+  $exclude_dict = []
   if File.exist? File.join CONFIG_DIR, "exclude.txt"
     exd = File.read File.join CONFIG_DIR, "exclude.txt"
     exd.each_line do |line|
       x, y = line.chomp.split("\t")
       next if !x || !y || x.empty? || y.empty?
-      EXCLUDE_DICT.push([x, y])
+      $exclude_dict.push([x, y])
     end
   end
 end
 
 def exclude_word? yomi, base
-  EXCLUDE_DICT.any? do |i|
+  $exclude_dict.any? do |i|
     File.fnmatch?(i[0], yomi) && File.fnmatch?(i[1], base)
   end
 end
