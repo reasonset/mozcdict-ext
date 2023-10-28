01490262fe58576b4356734468c973bc46acdd33
Masaki Haruka <yek@reasonset.net>
2023-10-28 17:07:30 +0900

    Update exclude funtion.

--- lib/dictutils.rb
+++ lib/dictutils.rb
@@ -1,10 +1,10 @@
 def load_exclude_dict
-  $config_dir = File.join((ENV["XDG_CONFIG_HOME"] || [ENV["HOME"], ".config"]), "mozcdic-ext")
+  $config_dir = File.join((ENV["XDG_CONFIG_HOME"] || [ENV["HOME"], ".config"]), "mozcdict-ext")
   $exclude_dict = []
   if File.exist? File.join $config_dir, "exclude.txt"
     exd = File.read File.join $config_dir, "exclude.txt"
     exd.each_line do |line|
-      x, y = line.chomp.split("\t")
+      x, y = line.strip.split(/\s+/, 2)
       next if !x || !y || x.empty? || y.empty?
       $exclude_dict.push([x, y])
     end
